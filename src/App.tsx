0ec39a84e901b4fa6738518399d547fc83dfb63b
Konrad Sosna <konrad.sosna.95@gmail.com>
2024-05-26 23:53:44 +0200

    Repo Init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import { ProcustTable } from './components/ProductTable';
+
+function App() {
+	return <ProcustTable />;
+}
+
+export default App;
---
+++ src/components/ProductTable.tsx
@@ -0,0 +1,5 @@
+import { memo } from 'react';
+
+export const ProcustTable = memo(() => {
+	return <div>Table</div>;
+});
